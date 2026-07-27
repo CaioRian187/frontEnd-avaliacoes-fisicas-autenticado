eb9340fa0adcc3069f5049c60691e707fa8bbf9d
CaioRian187 <caiorian246810@gmail.com>
2026-07-27 13:36:14 -0300

    feature: Adicionando as configurações de Layout da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { LayoutApp } from "./layouts/LayoutApp"
+import { RoutesApp } from "./RoutesApp"
+
 function App() {
   return (
-    <>
-
-    </>
+    <LayoutApp>
+      <RoutesApp />
+    </LayoutApp>
   )
 }
 
