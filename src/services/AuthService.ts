fda9cb7752ba4422070e6822168c61a285401fed
CaioRian187 <caiorian246810@gmail.com>
2026-07-27 13:38:02 -0300

    feature: Criando as páginas Login e cadastro

---
+++ src/services/AuthService.ts
@@ -0,0 +1,34 @@
+import axios from "axios"
+import type { LoginRequest } from "../interfaces/LoginRequest";
+import type { CadastroRequest } from "../interfaces/CadastroRequest";
+
+const API_URL = import.meta.env.VITE_URL_API;
+
+export const handleLogin = async (loginRequest: LoginRequest) => {
+
+    try {
+        const response = await axios.post(API_URL + "/auth/login", loginRequest);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+export const handleCadastro = async (cadastroRequest: CadastroRequest) => {
+
+    try {
+        const response = await axios.post(API_URL + "/auth/cadastro", cadastroRequest);
+        console.log(response.data)
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+
+}
+
+export const handleLogout = () => {
+    localStorage.removeItem("token");
+}
+
