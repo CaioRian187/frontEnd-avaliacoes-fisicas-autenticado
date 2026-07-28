5c29ad5cf9ed2b9a3ef3cca43a203c0e8ac3e0a6
CaioRian187 <caiorian246810@gmail.com>
2026-07-28 13:24:40 -0300

    fix: Adicionando alertas no método getTokenDecoded

--- src/utils/Auth.ts
+++ src/utils/Auth.ts
@@ -2,8 +2,12 @@ import { jwtDecode } from "jwt-decode";
 import type { UserToken } from "../interfaces/UserToken";
 
 export const getTokenDecoded = (): UserToken | null => {
+
     const token = localStorage.getItem("token");
-    if (!token) return null;
+    if (!token) {
+        alert("Token não encontrado. Por favor, faça login novamente.");
+        return null;
+    }
 
     try {
 
@@ -11,12 +15,14 @@ export const getTokenDecoded = (): UserToken | null => {
 
         if (decoded.exp && decoded.exp * 1000 < Date.now()) {
             localStorage.removeItem("token");
+            alert("Token expirado. Por favor, faça login novamente.");
             return null;
         }
         console.log(decoded);
         return decoded;
 
     } catch (error) {
+        alert("Token Inválido.");
         console.error("Token Inválido.", error);
         return null;
     }
