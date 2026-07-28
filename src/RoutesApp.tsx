09beb28dedb6711b99660b987131dee5cb021986
CaioRian187 <caiorian246810@gmail.com>
2026-07-28 09:48:30 -0300

    feature: Adicionando a roda circunferencias no RoutesApp

--- src/RoutesApp.tsx
+++ src/RoutesApp.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LoginPage } from "./pages/LoginPage";
 import { CasdastroPage } from "./pages/CadastroPage";
+import { CircunferenciasPage } from "./pages/CircunferenciasPage";
 
 
 export const RoutesApp = () => {
@@ -9,6 +10,7 @@ export const RoutesApp = () => {
             <Routes>
                 <Route path="/" element={<LoginPage />} />
                 <Route path="/cadastro" element={<CasdastroPage />} />
+                <Route path="/circunferencias" element={<CircunferenciasPage />} />
             </Routes>
         </BrowserRouter>
     );
